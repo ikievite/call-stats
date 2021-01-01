29c1cce9b8ad44ec9f5fe7dbf1dcce67ad953bd4
ikievite <ikievite@gmail.com>
2021-01-01 10:57:04 +0200

    update linter cfg

--- call_stats/stats/all_incoming_calls.py
+++ call_stats/stats/all_incoming_calls.py
@@ -4,10 +4,20 @@
 
 
 import datetime
+
 import requests
 
 
 def all_in_calls(key, secret):
+    """Fing all incoming calls since date.
+
+    Args:
+        key: for api
+        secret: for api
+
+    Returns:
+        json with all incoming calls since date
+    """
     url = 'https://api.binotel.com/api/4.0/stats/all-incoming-calls-since.json'
     date = datetime.date.today()
     time = datetime.time(0, 0)
