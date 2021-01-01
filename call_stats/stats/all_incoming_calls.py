4ba660bf452b40e40d74e5e3c81b9d26f153b5e0
ikievite <ikievite@gmail.com>
2021-01-01 20:28:30 +0200

    add optional argument to function all_in_calls -- timestamp

--- call_stats/stats/all_incoming_calls.py
+++ call_stats/stats/all_incoming_calls.py
@@ -8,27 +8,34 @@ import datetime
 import requests
 
 
-def all_in_calls(key, secret):
+def all_in_calls(key, secret, timestamp):
     """Fing all incoming calls since date.
 
     Args:
         key: for api
         secret: for api
+        timestamp: since that time
 
     Returns:
         json with all incoming calls since date
     """
     url = 'https://api.binotel.com/api/4.0/stats/all-incoming-calls-since.json'
-    date = datetime.date.today()
+
+    if timestamp == 'today':
+        date = datetime.date.today()
+    else:
+        date = datetime.date.fromisoformat(timestamp)
+
     time = datetime.time(0, 0)
     dt = datetime.datetime.combine(date, time)
-    timestamp = datetime.datetime.timestamp(dt)
+    stamp = datetime.datetime.timestamp(dt)
 
     payload = {
-        'timestamp': timestamp,
+        'timestamp': stamp,
         'key': key,
         'secret': secret,
     }
+
     if key and secret:
         output = requests.post(url, json=payload)
         return output.json()
