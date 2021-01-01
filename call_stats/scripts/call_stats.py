448c838fdc33699a12ec26a7a2006a7911c118cc
ikievite <ikievite@gmail.com>
2021-01-01 10:55:51 +0200

    remove comments

--- call_stats/scripts/call_stats.py
+++ call_stats/scripts/call_stats.py
@@ -23,24 +23,6 @@ def parse_arguments():
     arguments = parser.parse_args()
     return arguments
 
-'''
-def all_in_calls_today(key, secret):
-    url = 'https://api.binotel.com/api/4.0/stats/all-incoming-calls-since.json'
-
-    date = datetime.date.today()
-    time = datetime.time(0, 0)
-    dt = datetime.datetime.combine(date, time)
-    timestamp = datetime.datetime.timestamp(dt)
-
-    payload = {
-        'timestamp': timestamp,
-        'key': key,
-        'secret': secret,
-    }
-    if key and secret:
-        output = requests.post(url, json=payload)
-        return output.json()
-'''
 
 def main():  # noqa: WPS210
     """Run main func."""
