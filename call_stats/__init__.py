19342a27a011a3659fb2d8e12f829b206a686306
ikievite <ikievite@gmail.com>
2021-06-07 08:36:17 +0300

    add logger

--- call_stats/__init__.py
+++ call_stats/__init__.py
@@ -1,1 +1,27 @@
+
+
 """Call Stats Project."""
+
+
+import logging
+
+from call_stats.generate_stats import genstats
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# stderr
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '{asctime} - {levelname} - {name} - {message}', datefmt='%H:%M:%S', style='{',
+)
+
+console.setFormatter(formatter)
+
+logger.addHandler(console)
+
+
+__all__ = (  # noqa: WPS410
+    'genstats',
+)
